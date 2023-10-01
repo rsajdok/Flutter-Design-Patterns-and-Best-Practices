ba2cc41bf6e8d9fde94e1de330658db4bec77c9f
darjaorlova <darja.orlova@chi.lv>
2023-10-01 13:33:49 +0300

    CH13: Add an example rule to `analysis_options.yaml`

--- Chapter03/candy_store/lib/main.dart
+++ Chapter03/candy_store/lib/main.dart
@@ -1,15 +1,16 @@
-import 'package:candy_store/cart_view_model.dart';
-import 'package:candy_store/cart_view_model_provider.dart';
-import 'package:candy_store/main_page.dart';
-import 'package:flutter/material.dart';
+import "package:candy_store/cart_view_model.dart";
+import "package:candy_store/cart_view_model_provider.dart";
+import "package:candy_store/main_page.dart";
+import "package:flutter/material.dart";
 
 // At this point, all of the code is in the `lib` folder and we will sort it in Part 3
+// TODO: Merge changes from other chapters into this branch once they are done
 void main() {
   runApp(
     CartViewModelProvider(
       cartViewModel: CartViewModel(),
       child: MaterialApp(
-        title: 'Candy shop',
+        title: "Candy shop",
         theme: ThemeData(
           primarySwatch: Colors.lime,
         ),
