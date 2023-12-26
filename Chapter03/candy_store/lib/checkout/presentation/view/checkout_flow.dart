7b505057c046aa428487e7bc2c0aae84bb813b38
darjaorlova <dariaorlova.bbapps@gmail.com>
2023-12-26 22:21:40 +0100

    CH8: Scoped checkout flow via GetIt

--- Chapter03/candy_store/lib/checkout/presentation/view/checkout_flow.dart
+++ Chapter03/candy_store/lib/checkout/presentation/view/checkout_flow.dart
@@ -1,8 +1,5 @@
-import 'package:candy_store/checkout/data/repository/stub_checkout_repository.dart';
-import 'package:candy_store/checkout/domain/repository/checkout_repository.dart';
 import 'package:candy_store/checkout/presentation/view/checkout_page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CheckoutFlow extends StatefulWidget {
   const CheckoutFlow({super.key});
@@ -14,10 +11,6 @@ class CheckoutFlow extends StatefulWidget {
 class _CheckoutFlowState extends State<CheckoutFlow> {
   @override
   Widget build(BuildContext context) {
-    return MultiRepositoryProvider(providers: [
-      RepositoryProvider<CheckoutRepository>(
-        create: (context) => StubCheckoutRepository(),
-      ),
-    ], child: CheckoutPage.withBloc());
+    return CheckoutPage.withBloc();
   }
 }
