090f32b9253deaaf11b190aac2abb2b1424fea1e
darjaorlova <dariaorlova.bbapps@gmail.com>
2023-12-26 22:01:17 +0100

    CH8: Scoped checkout flow via flutter_bloc

---
+++ Chapter03/candy_store/lib/checkout/presentation/view/checkout_page.dart
@@ -0,0 +1,48 @@
+import 'package:candy_store/checkout/presentation/bloc/checkout_cubit.dart';
+import 'package:candy_store/checkout/presentation/bloc/checkout_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CheckoutPage extends StatefulWidget {
+  const CheckoutPage({super.key});
+
+  @override
+  State<CheckoutPage> createState() => _CheckoutPageState();
+
+  static Widget withBloc() {
+    return BlocProvider<CheckoutCubit>(
+      create: (context) => CheckoutCubit(
+        context.read(),
+      )..loadPaymentMethods(),
+      child: const CheckoutPage(),
+    );
+  }
+}
+
+class _CheckoutPageState extends State<CheckoutPage> {
+  late final CheckoutCubit _cubit;
+
+  @override
+  void initState() {
+    super.initState();
+    _cubit = context.read();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<CheckoutCubit, CheckoutState>(builder: (context, state) {
+      return Scaffold(
+        body: Center(
+          child: TextButton(
+            onPressed: () {
+              final paymentMethod =
+                  state.paymentMethods.firstOrNull ?? 'Unknown';
+              _cubit.checkout(paymentMethod);
+            },
+            child: const Text('Checkout'),
+          ),
+        ),
+      );
+    });
+  }
+}
